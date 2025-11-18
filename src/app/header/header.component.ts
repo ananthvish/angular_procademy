8eebb5a5e68856e3cc99db757e01dfebff4c973b
Ananth <ananth.vishu@gmail.com>
2025-11-18 19:06:43 +0530

    initial commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+  // styles: ['a{text-decoration: none; margin: 0 10px;}', 'button{}']
+})
+export class HeaderComponent {}
