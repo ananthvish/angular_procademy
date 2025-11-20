d8356c0d12f294450082b8af7bb5fc79e76f290b
Ananth <ananth.vishu@gmail.com>
2025-11-20 16:34:56 +0530

    added content projection

--- src/app/container/product-detail/product-detail.component.ts
+++ src/app/container/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ProductListComponent } from '../product-list/product-list.component';
+import { Product } from 'src/app/models/Product';
 
 @Component({
   selector: 'product-detail',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent {
+
+  @Input()
+  productListComponent: ProductListComponent = undefined;
+
+  product: Product;
+
+  ngOnInit() { // all the properties are initialised
+     this.product = this.productListComponent.selectedProduct;
+  }
 
 }
--- src/app/container/search/search.component.ts
+++ src/app/container/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -12,13 +12,16 @@ export class SearchComponent {
   @Output()
   searchTextEvent: EventEmitter<string> = new EventEmitter<string>();
 
+  @ViewChild('searchInput')
+  searchInputEl: ElementRef;
+
   // searchTextChanged() {
   //   console.log(this.searchText)
   //   this.searchTextEvent.emit(this.searchText);
   // }
 
-  updateSearchText(inputEl: HTMLInputElement) {
-    this.searchText = inputEl.value;
+  updateSearchText() {
+    this.searchText = this.searchInputEl.nativeElement.value;
     this.searchTextEvent.emit(this.searchText);
   }
 
